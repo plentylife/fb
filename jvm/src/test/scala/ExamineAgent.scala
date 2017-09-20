f611cbb90d05d269c522b43936b34527b49ea24f
anton <antonkats@gmail.com>
2017-09-20 16:29:40 -0400

    updates to tests mainly using tests to modify records from server

--- jvm/src/test/scala/ExamineAgent.scala
+++ jvm/src/test/scala/ExamineAgent.scala
@@ -1,125 +1,189 @@
-import fb.{FbAgent, FbSettings, UserInfo}
+import fb.{FbSettings, UserInfo}
 import org.scalatest.{FreeSpec, Matchers}
-import plenty.agent.Accounting
-import plenty.state.StateManager
 import plenty.TestUtilities._
+import plenty.agent.{Accounting, AgentPointer}
 import plenty.network.MintPress
+import plenty.state.StateManager
+import plenty.state.model.{BidTransaction, Node, TransactionType}
 
 import scala.language.postfixOps
+import com.softwaremill.quicklens._
+import fb.donation.DonationResponses
 
 class ExamineAgent extends FreeSpec with Matchers {
 
-  "Single agent" in {
-//    val agent = StateManager.load("1495520050542318", "onserver/") // gyorgy
-    val agent = StateManager.load("1624828950901835", "onserver/").get
+  "Analyzing agents" - {
+    "Single agent" in {
+      //    val agent = StateManager.load("1495520050542318", "onserver/") // gyorgy
+      val agent = StateManager.load("1624828950901835", "onserver/").get
 
-    println(s"all coins count ${agent.state.coins.size}")
-    println(s"my coin coint is ${Accounting.getOwnCoins(agent)}")
-    println()
-    val transWithMe = agent.state.chains.transactions.filter(t ⇒ t.to == agent.node || t.from == agent.node)
-    transWithMe.foreach(t ⇒ pprint(t))
-    println()
-    println("coins that used to belong to me")
-    val myOldCoins = transWithMe.head.coins intersect agent.state.coins
-    val myOldCoinIds = myOldCoins map {_.id}
-    (agent.state.coins filter {c ⇒ myOldCoinIds contains c.id}).foreach(println)
-    val transactionsThatContainMyCoins = agent.state.chains.transactions
-      .map(t ⇒ t → (t.coins intersect myOldCoins)).filter(_._2.nonEmpty).map(_._1)
-    println("trans with my coins")
-    transactionsThatContainMyCoins foreach {t ⇒
-      pprint(t); println(t.id); println(t.coins.map{_.id});println(t.transactionType);println()}
+      println(s"all coins count ${agent.state.coins.size}")
+      println(s"my coin coint is ${Accounting.getOwnCoins(agent)}")
+      println()
+      val transWithMe = agent.state.chains.transactions.filter(t ⇒ t.to == agent.node || t.from == agent.node)
+      transWithMe.foreach(t ⇒ pprint(t))
+      println()
+      println("coins that used to belong to me")
+      val myOldCoins = transWithMe.head.coins intersect agent.state.coins
+      val myOldCoinIds = myOldCoins map {_.id}
+      (agent.state.coins filter { c ⇒ myOldCoinIds contains c.id }).foreach(println)
+      val transactionsThatContainMyCoins = agent.state.chains.transactions
+        .map(t ⇒ t → (t.coins intersect myOldCoins)).filter(_._2.nonEmpty).map(_._1)
+      println("trans with my coins")
+      transactionsThatContainMyCoins foreach { t ⇒
+        pprint(t); println(t.id); println(t.coins.map {_.id}); println(t.transactionType); println()
+      }
+    }
+
+    "FB Agent" in {
+      println("\n\n")
+      val agent = StateManager.load("facebook_agent").get
+
+      println("coins that do not belong to fb agent")
+      val notMine = agent.state.coins.filterNot(_.belongsTo == agent.node)
+      notMine foreach println
+      println(s"coins that do ${agent.state.coins.count(_.belongsTo == agent.node)}")
+
+      println(agent.state.coins)
+    }
+
+    "All server agents" in {
+      FbSettings.prod = true
+
+      val as = StateManager.loadAll("onserver/")
+      val fbAgent = as find (_.id == "facebook_agent") get;
+
+      println(s"Fb agent has ${Accounting.getSelfBalance(fbAgent)}")
+
+      as filterNot {_.id == "facebook_agent"} foreach { a =>
+
+        val ui = UserInfo.get(a.id)
+        val balanceSelf = Accounting.getBalance(a.node)(a)
+        val balanceFb = Accounting.getBalance(a.node)(fbAgent)
+
+        div()
+        println(s"${a.id} \t ${ui.name} ${balanceSelf} $balanceFb")
+
+        transactionsInvloving(a) foreach pprint
+//        transactionsInvloving(a) filterNot (_.transactionType == TransactionType.DEMURAGE) foreach pprint
+      }
+
+      div("donations")
+      as flatMap {_.state.donations} foreach {d ⇒
+        println(d.title)
+        println(d.attachments mkString("\n"))
+      }
+    }
   }
 
-  "FB Agent" in {
-    println("\n\n")
-    val agent = StateManager.load("facebook_agent").get
+  "Using server agents" - {
+    "Send bubbles" in {
+      FbSettings.prod = true
+      val as = StateManager.loadAll("onserver/")
+      val antonNode = Node("767613720030082")
 
-    println("coins that do not belong to fb agent")
-    val notMine = agent.state.coins.filterNot(_.belongsTo == agent.node)
-    notMine foreach println
-    println(s"coins that do ${agent.state.coins.count(_.belongsTo == agent.node)}")
-
-    println(agent.state.coins)
-  }
-
-  "All server agents" in {
-    FbSettings.prod = true
-
-    val as = StateManager.loadAll("onserver/")
-    val fbAgent = as find(_.id == "facebook_agent") get;
-
-    as filterNot {_.id == "facebook_agent"} foreach { a =>
-      val ui = UserInfo.get(a.id)
-      val balanceSelf = Accounting.getBalance(a.node)(a)
-      val balanceFb = Accounting.getBalance(a.node)(fbAgent)
-
-      println(s"\t ${a.id}")
-      println(s"${ui.name} ${balanceSelf} $balanceSelf")
+      as find (_.node == antonNode) foreach {a ⇒
+        val p = new AgentPointer(a)
+        a.state.donations foreach {d ⇒
+          DonationResponses.showDonationBubble(p, d, Some(d.id))
+        }
+      }
     }
   }
 
   "Modifying server agents" - {
-    import com.softwaremill.quicklens._
     FbSettings.prod = true
-    val as = StateManager.loadAll("onserver/")
+    var as = StateManager.loadAll("onserver/")
 
-    as filterNot (_.id == "facebook_agent") foreach {a ⇒
-      val ui = UserInfo.get(a.id)
-      println(ui.name, a.id)
-    }
-
-    "Removing bids" in {
-      div("bids")
-      as foreach {a ⇒
-        println(a.state.bids)
-        val u = a.modify(_.state.bids).using(_.filterNot(_.by.id == "1624828950901835"))
-        StateManager.save(u, "onserver/")
+    "Outputting basic info" in {
+      as filterNot (_.id == "facebook_agent") foreach { a ⇒
+        val ui = UserInfo.get(a.id)
+        println(ui.name, a.id)
       }
-      Thread.sleep(100)
-    }
-
-    "Removing transactions" in {
-      div("transactions")
-      as foreach {a ⇒
-        println(s"\n${a.id}")
-        a.state.chains.transactions.foreach {pprint}
-        val u = a.modify(_.state.chains.transactions).using(_ ⇒ List())
-        StateManager.save(u, "onserver/")
-      }
-      Thread.sleep(100)
     }
 
     "Adding coins" in {
-      div("minting")
       val allCoins = MintPress.fillCoinSet(Set(), null).grouped(7).toList
 
-      val distributedCoins = as filterNot (_.id == "facebook_agent") zip allCoins flatMap {case (a, cs) ⇒
+      val distributedCoins = as filterNot (_.id == "facebook_agent") zip allCoins flatMap { case (a, cs) ⇒
         cs map {_.modify(_.belongsTo).using(_ ⇒ a.node)}
       }
 
-      distributedCoins foreach println
+      div("minting")
+      println(distributedCoins mkString ("\n"))
 
-      as foreach {a ⇒
+      as foreach { a ⇒
         val u = a.modify(_.state.coins).using(_ ⇒ distributedCoins)
         StateManager.save(u, "onserver/")
       }
 
-      Thread.sleep(100)
     }
 
     "Checking coins" in {
       div("coin check")
       val check = StateManager.loadAll("onserver/")
 
-      check filterNot (_.id == "facebook_agent") foreach {a1 ⇒
-        check foreach {a2 ⇒
+      check filterNot (_.id == "facebook_agent") foreach { a1 ⇒
+        check foreach { a2 ⇒
           val b = Accounting.getBalance(a1.node)(a2)
-          println(b)
           b shouldBe 7
         }
       }
+
+      Accounting.getSelfBalance({check find(_.id == "facebook_agent") get}) should be (0)
     }
 
 
+    "making sure that the correct transactions are kept" in {
+      as = StateManager.loadAll("onserver/")
+
+      val antonNode = Node("767613720030082")
+      val sarahNode = Node("1843592592335659")
+      // selecting bid transactions that should be kept
+      val bidTransactions = as flatMap {_.state.chains.transactions} collect { case t: BidTransaction ⇒ t }
+
+      val sarahsBuy = bidTransactions filter { t ⇒
+        t.from.id == "1843592592335659" && t.to.id == "767613720030082" &&
+          t.coins.size == 3
+      }
+      sarahsBuy should have size (1)
+
+      div("sarahs transaction")
+      pprint(sarahsBuy.head)
+
+      var transactionToKeep = sarahsBuy.head
+      val sarahsCoins = Accounting.getOwnCoins(
+        {as find (_.node == transactionToKeep.from) get}) take transactionToKeep.coins.size
+      sarahsCoins forall {_.belongsTo == sarahNode} shouldBe true
+      transactionToKeep = transactionToKeep.modify(_.coins).using(_ ⇒ sarahsCoins)
+      val antonsCoins = Accounting.transferCoins(transactionToKeep)
+
+      transactionToKeep.coins forall {_.belongsTo == sarahNode} shouldBe true
+      antonsCoins forall (_.belongsTo.id == "767613720030082") shouldBe true
+
+      div("coin update")
+      as foreach { a ⇒
+        println("modifying", a.id)
+        var upd = a.modify(_.state.coins).using {c ⇒
+          val stage1 = c diff antonsCoins
+          stage1 ++ antonsCoins
+        }
+        upd = upd.modify(_.state.chains.transactions).using(_ ⇒ List(transactionToKeep))
+        StateManager.save(upd, "onserver/")
+      }
+
+      var asCheck = StateManager.loadAll("onserver/")
+      asCheck forall { a ⇒
+        println(a.id, Accounting.getBalance(antonNode)(a))
+        Accounting.getBalance(antonNode)(a) == 10 } shouldBe true
+      asCheck forall { a ⇒ Accounting.getBalance(sarahNode)(a) == 4 } shouldBe true
+      val others = asCheck filterNot { a ⇒ a.node == antonNode || a.node == sarahNode || a.id == "facebook_agent" }
+      others forall {
+        a ⇒ Accounting.getSelfBalance(a) == 7
+      } shouldBe true
+      asCheck forall {_.state.chains.transactions.exists {t ⇒
+        t.from == sarahNode && t.to == antonNode && t.coins.size == 3
+      }} shouldBe true
+  }
   }
 }
--- jvm/src/test/scala/plenty/TestUtilities.scala
+++ jvm/src/test/scala/plenty/TestUtilities.scala
@@ -2,6 +2,7 @@ package plenty
 
 import java.util.Date
 
+import fb.UserInfo
 import plenty.agent.model.Agent
 import plenty.network.{Message, Network, SendReceiveInterface}
 import plenty.state.model.Transaction
@@ -23,11 +24,23 @@ object TestUtilities {
     }
   }
 
+  def getName(id: String) = id match {
+    case "facebook_agent" ⇒ id
+    case id ⇒ UserInfo.get(id).name
+  }
+
   def pprint(t: Transaction) = {
-    println(s"${t.from.id} -> ${t.to.id} ${t.coins.size}\t ${t.transactionType} \t${new Date(t.timestamp)}")
+    val nameFrom = getName(t.from.id)
+    val nameTo = getName(t.to.id)
+    println(s"${nameFrom} -> ${nameTo} " +
+      s"${t.coins.size}\t ${t.transactionType} \t${new Date(t.timestamp)}")
   }
 
   def div(mark: String = "") = println(s"\n\n===\t$mark\n")
+
+  def transactionsInvloving(a: Agent) = a.state.chains.transactions.filter(t ⇒ {
+    t.from == a.node || t.to == a.node
+  })
 }
 
 object MockSendReceiveInterface extends SendReceiveInterface {
